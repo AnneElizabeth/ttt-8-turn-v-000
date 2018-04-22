ba69f86453cccc5b5e73dd1dd469cce9d279e0ac
Anne Edmondson <edmondson.anne@gmail.com>
2018-04-22 02:23:36 +0000

    automatically backed up by learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -40,7 +40,7 @@ def turn(board)
   index = input_to_index(user_input)
 end
 
-
+board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
  
 if valid_move?(board, index)
   move(board, index, value = "X")
